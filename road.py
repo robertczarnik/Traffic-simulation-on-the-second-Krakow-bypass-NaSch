5aa72447d36b523aa4e46b36386c3e274d02ca56
Robert Czarnik <robert.czarnik98@gmail.com>
2019-05-15 23:09:44 +0200

    3 main roads and one crossing them. Vehicles can turn right on crossing.

--- road.py
+++ road.py
@@ -32,7 +32,7 @@ class Vehicle(object):
         self.position=position
         self.road.lane[self.position].vehicle=1
         
-    def updateCell(self,x):
+    def update_cell(self,x):
         self.road.lane[self.position].vehicle=x
     
     def get_speed_limit(self):
@@ -43,37 +43,75 @@ class Vehicle(object):
             return False
         
         return self.road.lane[self.position+distance].vehicle==1
+    
+    def check_crossing(self,distance):
+        if(self.road.lane[self.position+distance].crossing_id!=None): #czyli jest jakies skrzyzowanie drog
+            return [self.road.other_roads[self.road.lane[self.position+distance].crossing_id-1],self.road.lane[self.position+distance].index] #zwracam droge ktora sie krzyzuje i pozycje w niej tego miejsca
+        return None
         
 
 #representing roads
-road1_len=55
-road2_len=22
-road1 = [0] * road1_len
-road2 = [0] * road2_len
+main_road1_len=55
+main_road2_len=55
+main_road3_len=55
 
+road1_len=22
+
+main_road1 = [0] * main_road1_len
+main_road2 = [0] * main_road2_len
+main_road3 = [0] * main_road3_len
+
+road1 = [0] * road1_len
+
+
+for i in range(main_road1_len):
+    main_road1[i]=Cell(0,False);
+    
+for i in range(main_road2_len):
+    main_road2[i]=Cell(0,False);
+    
+for i in range(main_road3_len):
+    main_road3[i]=Cell(0,False);
+    
+    
 for i in range(road1_len):
     road1[i]=Cell(0,False);
-    
-for i in range(road2_len):
-    road2[i]=Cell(0,False);
 
-road1[24]=Cell(0,True,1,6);
-road2[6]=Cell(0,True,1,24);
 
-road1 = Road(road1,False,False)
-road2 = Road(road2,False,False)
+main_road1[24]=Cell(0,True,1,13); #dla kazdej z tych drog to jest pierwsze skrzyzowanie
+main_road2[24]=Cell(0,True,1,14);
+main_road3[24]=Cell(0,True,1,15);
 
-road1.other_roads=[road2]
-road2.other_roads=[road1]
+road1[13]=Cell(0,True,1,24); #tutaj juz ta droga krzyzuje sie po kolei z trzema innymi
+road1[14]=Cell(0,True,2,24);
+road1[15]=Cell(0,True,3,24);
+
+main_road1 = Road(main_road1,None,main_road2)
+main_road2 = Road(main_road2,main_road1,main_road3)
+main_road3 = Road(main_road3,main_road2,None)
+
+road1 = Road(road1)
+
+main_road1.other_roads=[road1]#dodanie drog z ktorymi sie po kolei krzyzuja
+main_road2.other_roads=[road1]
+main_road3.other_roads=[road1]
+
+road1.other_roads=[main_road1,main_road2,main_road3]
 #--
 
 
-
 vehicles=[] #list of vehicles that are on road
-vehicles.append(Vehicle(0,road1,0)) #dodanie pojazdu do road1 na miejscu 0 z predkoscia poczatkowa 0
+vehicles.append(Vehicle(0,main_road1,0)) #dodanie pojazdu do road1 na miejscu 0 z predkoscia poczatkowa 0
+vehicles.append(Vehicle(0,main_road2,0))
+vehicles.append(Vehicle(0,main_road3,0))
+
+def print_roads():
+    main_road1.print_road()
+    main_road2.print_road()
+    main_road3.print_road()
 
 clear()
-road1.print_road()
+print_roads()
 
 for j in range(30): #mainloop
     vehicles_down=[]
@@ -89,25 +127,44 @@ for j in range(30): #mainloop
         if(random.randint(0, 9)<4): #Randomisation 40% chance to slow down
             vehicles[i].velocity-=1
             
+              
+        vehicles[i].update_cell(0) #remove vehicle from previous position
         
-        vehicles[i].updateCell(0)
         if( (vehicles[i].position+vehicles[i].velocity) >= len(vehicles[i].road.lane)):  #wyjechanie poza droge
             vehicles_down.append(i) #indeksy pojazdow ktore trzeba usunac
-        else: #Driving
-            vehicles[i].position+=vehicles[i].velocity
-            vehicles[i].updateCell(1)
+            continue
+        
+        for k in range(1,vehicles[i].velocity+1): #sprawdzenie czy nie ma jakiegos skrzyzowania przed pojazdem
+            road_info=vehicles[i].check_crossing(k)
+            if(road_info!=None): #jest jakies skrzyzowanie
+                #print("SKRZYZOWANIE")
+                if(road_info[0].lane[road_info[1]].vehicle == 1): #sprawdzam czy jest tam pojazd
+                    vehicles[i].velocity=k-1 #jesli jest to zmieniejszam predkosc tak zeby w niego nie wjechac
+                else: #jesli nie ma to musze podjac decyzje czy skrecic - poki co szansa 50%
+                    if(random.randint(0, 9)<5):
+                        vehicles[i].road=road_info[0]
+                        vehicles[i].position=road_info[1]
+                        vehicles[i].update_cell(1) #na jednej warstwie widac tylko ta jedynke(ten pojazd)
+                        break
+                  
+        
+         
+        vehicles[i].position+=vehicles[i].velocity #Driving
+        vehicles[i].update_cell(1)
             
      
-    for w in range(len(vehicles_down)): #usuwanie pojazdow ktore wyjechaly poza droge
+    for w in range(len(vehicles_down)-1,-1): #usuwanie pojazdow ktore wyjechaly poza droge (usuwanie od tylu zeby nie bylo bledow z odwolaniem do nieistniejacego pojazdu)
+        print("do wywalenia: ",end='')
+        print(vehicles_down[w])
         vehicles.pop(vehicles_down[w])
     
-    if(road1.lane[0].vehicle==0): #dodaj samochod do drogi
-        vehicles.append(Vehicle(0,road1,0))        
+    #if(road1.lane[0].vehicle==0): #dodaj samochod do drogi
+    #    vehicles.append(Vehicle(0,main_road1,0))        
         
     time.sleep(1)
 
     clear()
-    road1.print_road()
+    print_roads()
     
 
 
